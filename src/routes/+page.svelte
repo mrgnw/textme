5d1c2d50878532293901f18c6f97a7116798d412
mrgnw <2504532+mrgnw@users.noreply.github.com>
2023-03-09 19:31:09 +0000

    icons & stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,107 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	// todo: read clipboard on mount
+
+	function norm(s, country_code = '1') {
+		// remove leading zeros from s
+		// remove any non-digit, non-plus characters
+		country_code = country_code.replace(/^0+/, '').replace(/[^0-9]/g, '');
+		// remove leading zeros from s
+		const phone_digits = s.replace(/^0+/, '').replace(/[^0-9+]/g, '');
+		let last10 = phone_digits.slice(-10);
+		// if last10 is 8 digits, add 11 to the front
+		if (last10.length === 8) {
+			last10 = `11${last10}`;
+		}
+		if (last10.length !== 10) {
+			return '';
+		}
+
+		// get everything in the orignal string that isn't in the last10
+		const prefix = phone_digits.slice(0, -10);
+
+		// strip + from prefix
+		const strippedPrefix = prefix.replace(/^\+/, '');
+
+		let result = '';
+
+		// if strippedPrefix is digits, add country code
+		if (strippedPrefix.match(/^\d+$/)) {
+			result = `+${strippedPrefix}${last10}`;
+		} else {
+			result = `+${country_code}${last10}`;
+		}
+		console.debug(result);
+		return result;
+	}
+	function setCountryCode(code) {
+		country_code = code;
+	}
+
+	let value = '';
+	let normed = '';
+	let country_code = '54';
+
+	$: normed = norm(value, country_code);
+	$: ready = normed.length > 0;
+</script>
+
+<div class="flags">
+	<span on:click={() => setCountryCode('1')}>🇺🇸</span>
+	<span on:click={() => setCountryCode('52')}>🇲🇽</span>
+	<span on:click={() => setCountryCode('54')}>🇦🇷</span>
+	<span on:click={() => setCountryCode('55')}>🇧🇷</span>
+</div>
+<input bind:value={country_code} placeholder="country code" size="3" />
+<input bind:value type="tel" placeholder="11 2222 3333" size="12"/>
+
+<div class="wrappa">
+	<div class={ready ? 'flags' : 'not-ready'}>
+		<ul>
+			<li>
+				<a href="sms:+{normed}">
+					<img src="/sms.svg" height="24px" alt="Telegram" /></a>
+				
+			</li>
+			<li>
+				<a href="https://t.me/{normed}" target="_blank" rel="noreferrer">
+					<img src="/telegram.svg" height="24px" alt="Telegram" /></a
+				>
+			</li>
+			<li>
+				<a href="https://wa.me/{normed}" target="_blank" rel="noreferrer">
+					<img src="/WhatsApp.svg" height="24px" alt="Fucking WhatsApp, god help us all" />
+				</a>
+			</li>
+		</ul>
+	</div>
+</div>
+
+<style>
+	.wrappa {
+		min-height: 2em;
+	}
+	.flags {
+		display: flex;
+		flex-direction: row;
+		cursor: pointer;
+		font-size: 2em;
+	}
+	.not-ready {
+		display: none;
+	}
+	span {
+		-webkit-touch-callout: none;
+		-webkit-user-select: none;
+		-khtml-user-select: none;
+		-moz-user-select: none;
+		-ms-user-select: none;
+		user-select: none;
+	}
+	ul {
+		display: flex;
+		/* flex-direction: row; */
+		list-style: none;
+		padding: 0;
+		margin: 0;
+	}
+</style>
