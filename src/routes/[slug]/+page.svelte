acd3a430c8fb091dfe2fd8771436e569cb17e487
Morgan <2504532+mrgnw@users.noreply.github.com>
2024-10-07 05:13:38 +0200

    replaceDigitWords on slug

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -1,8 +1,11 @@
 <script>
 	import { page } from '$app/stores';
 	import PhoneLinksCard from "$components/PhoneLinksCard.svelte";
+	import { replaceDigitWords } from '$lib/normalize.js';
 
 	let initialValue = $page.params.slug;
+	initialValue = replaceDigitWords(initialValue);
+
 </script>
 
 <PhoneLinksCard {initialValue} />
